f970bba83406400a2a919a313c0ab20712f214f8
realme-mt6768-devs <rulsrender2@gmail.com>
2024-10-11 12:43:38 +0700

    Update and rename lineage_even.mk to arrow_even.mk

--- arrow_even.mk
+++ arrow_even.mk
@@ -7,21 +7,24 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/realme/even/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_even
+PRODUCT_NAME := arrow_even
 PRODUCT_DEVICE := even
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme C25/C25s/Narzo 50A
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# ArrowOS additions
+DEVICE_MAINTAINER := ismasrull
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=even \
